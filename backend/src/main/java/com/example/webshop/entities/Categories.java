0f42d2014fbfc5b0a337f43ea6f5f071593e7aef
mistrzegiptu <mistrzegiptu@gmail.com>
2026-01-10 14:31:09 +0100

    Add product table

---
+++ backend/src/main/java/com/example/webshop/entities/Categories.java
@@ -0,0 +1,35 @@
+package com.example.webshop.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity(name = "Categories")
+public class Categories {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
+    private UUID categoryId;
+
+    @Column(name = "name")
+    private String name;
+
+    public Categories() {
+    }
+
+    public UUID getCategoryId() {
+        return categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
